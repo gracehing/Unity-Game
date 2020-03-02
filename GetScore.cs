dd85675e8aee80690b61f50ccb35f0eae6582232
gracehing <61696998+gracehing@users.noreply.github.com>
2020-03-02 22:52:28 +0800

    Create GetScore.cs

---
+++ GetScore.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetScore : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int theScore;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
+    }
+}
