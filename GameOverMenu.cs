1d5d5a4814d9890911a3e059dd3104902431b648
gracehing <61696998+gracehing@users.noreply.github.com>
2020-03-02 22:50:22 +0800

    Create GameOverMenu.cs

---
+++ GameOverMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
