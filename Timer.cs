b894abd62f04c619cbba77aab23d390d5e968085
gracehing <61696998+gracehing@users.noreply.github.com>
2020-03-02 22:55:22 +0800

    Create Timer.cs

---
+++ Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    float totalTime = 10f; //5 minutes
+
+    private void Update()
+    {
+        totalTime -= Time.deltaTime;
+        UpdateLevelTimer(totalTime);
+    }
+
+    public void UpdateLevelTimer(float totalSeconds)
+    {
+        int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+        int seconds = Mathf.RoundToInt(totalSeconds % 60f);
+
+        string formatedSeconds = seconds.ToString();
+
+        if (seconds == 60)
+        {
+            seconds = 0;
+            minutes += 1;
+        }
+
+        if (minutes == 00 && seconds == 00)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
